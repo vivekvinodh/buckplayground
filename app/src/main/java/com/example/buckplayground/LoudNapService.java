16eed7e987c06a8a0da45f42de577c64675f777f
Vivek Vinodh <5590649+vivekvinodh@users.noreply.github.com>
2019-05-28 17:00:22 -0700

    Loud Nap

---
+++ app/src/main/java/com/example/buckplayground/LoudNapService.java
@@ -0,0 +1,16 @@
+package com.example.buckplayground;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class LoudNapService extends Service {
+    public LoudNapService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
